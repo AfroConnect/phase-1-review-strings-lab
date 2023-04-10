21504b155a2ced950e8a3131f5b49b6ee32206c1
AfroConnect <126961726+AfroConnect@users.noreply.github.com>
2023-04-10 00:26:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Theophilus Appiah-Mensah';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreetingMod = `Welcome, ${currentUser.slice(0, 5)}!`;
